e1659ad45096d1126b68eba06e7172bc1700fb82
giang.hoang <giang.hoang@pythagoras.se>
2024-05-28 19:29:46 +0700

    init project

---
+++ src/lib/components/Header.svelte
@@ -0,0 +1,15 @@
+<script>
+	import HeaderItem from './HeaderItem.svelte';
+</script>
+
+<header>
+	<nav class="primary-nav">
+		<ul class="nav-list flex justify-center gap-3 font-bodyCap py-5">
+			<HeaderItem color="red-500" href="/">HOME</HeaderItem>
+			<HeaderItem color="orange-500" href="/article">ARTICLE</HeaderItem>
+			<HeaderItem color="purple-500" href="/about">ABOUT</HeaderItem>
+			<HeaderItem color="blue-500" href="/newsletter">NEWSLETTER</HeaderItem>
+			<HeaderItem color="green-500" href="/contact">CONTACT</HeaderItem>
+		</ul>
+	</nav>
+</header>
---
+++ src/lib/components/HeaderItem.svelte
@@ -0,0 +1,33 @@
+<script>
+	import { twMerge } from 'tailwind-merge';
+	import { page } from '$app/stores';
+	export let href = '';
+	export let clazz = '';
+	export let color = '';
+</script>
+
+<li>
+	<a
+		{href}
+		class={twMerge(
+			'text-xl border-b-4 border-solid pb-0.5',
+			`text-${color}`,
+			`border-${color}`,
+			clazz
+		)}
+		class:active={$page.url.pathname === href}>
+		<slot />
+	</a>
+</li>
+
+<style>
+	a {
+		border-bottom-left-radius: 15px 3px;
+		border-bottom-right-radius: 15px 5px;
+	}
+	a.active {
+		text-shadow:
+			0.5px 0px currentColor,
+			-0.5px 0px currentColor;
+	}
+</style>
