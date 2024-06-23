924e87c322286da2b1e9876cd24333b90161f921
giang hoang <13800000+gianglaodai@users.noreply.github.com>
2024-06-24 03:44:59 +0700

    udpate style format

--- src/lib/components/ui/button/index.js
+++ src/lib/components/ui/button/index.js
@@ -1,23 +1,19 @@
 import { tv } from 'tailwind-variants';
 import Root from './button.svelte';
 const buttonVariants = tv({
-	base: `inline-flex items-center justify-center whitespace-nowrap leading-6 font-medium ring-offset-background
-border-2 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring
-rounded-bl-[0.9375rem_15.9375rem] rounded-br-[14.0625rem_0.9375rem] rounded-tl-[15.9375rem_0.9375rem] rounded-tr-[0.9375rem_14.0625rem]
-transition-all duration-300 ease-in-out shadow-[15px_28px_25px_-18px_rgba(0,0,0,0.2)]
-focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50`,
+	base: 'inline-flex items-center justify-center whitespace-nowrap rounded-bl-[0.9375rem_15.9375rem] rounded-br-[14.0625rem_0.9375rem] rounded-tl-[15.9375rem_0.9375rem] rounded-tr-[0.9375rem_14.0625rem] border-2 font-medium leading-6 shadow-[15px_28px_25px_-18px_rgba(0,0,0,0.2)] ring-offset-background transition-all duration-300 ease-in-out focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50',
 	variants: {
 		variant: {
-			default: 'bg-primary text-primary-foreground border-primary-border hover:bg-primary/90',
+			default: 'border-primary-border bg-primary text-primary-foreground hover:bg-primary/90',
 			secondary:
-				'bg-secondary text-secondary-foreground border-secondary-border hover:bg-secondary/80',
-			success: 'bg-success text-success-foreground border-success-foreground hover:bg-success/90',
-			warning: 'bg-warning text-warning-foreground border-warning-foreground hover:bg-warning/90',
-			danger: 'bg-danger text-danger-foreground border-danger-foreground hover:bg-danger/90',
+				'border-secondary-border bg-secondary text-secondary-foreground hover:bg-secondary/80',
+			success: 'border-success-foreground bg-success text-success-foreground hover:bg-success/90',
+			warning: 'border-warning-foreground bg-warning text-warning-foreground hover:bg-warning/90',
+			danger: 'border-danger-foreground bg-danger text-danger-foreground hover:bg-danger/90',
 			destructive:
-				'bg-destructive text-destructive-foreground border-destructive-foreground hover:bg-destructive/90',
+				'border-destructive-foreground bg-destructive text-destructive-foreground hover:bg-destructive/90',
 			outline:
-				'bg-background text-primary border-primary hover:bg-accent hover:text-accent-foreground',
+				'border-primary bg-background text-primary hover:bg-accent hover:text-accent-foreground',
 			ghost: 'hover:bg-accent hover:text-accent-foreground',
 			link: 'text-primary underline-offset-4 hover:underline'
 		},
